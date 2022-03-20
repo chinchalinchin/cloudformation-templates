680c529be6c2d914ea4ef6f8a55a976073aa3da7
Grant Moore <gmoore@makpar.com>
2022-03-20 19:50:49 +0000

    OM-62 configuring deployment script

--- deploy/deployer.py
+++ deploy/deployer.py
@@ -5,7 +5,12 @@
 # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/cloudformation.html#CloudFormation.Client.update_stack
 
 import os
+import pprint
 import yaml
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
 
 import settings
 from logger import get_logger
@@ -17,9 +22,15 @@ log = get_logger('innolab-cloudformation.deploy.deployer')
 def get_deployment():
     if os.path.exists(settings.DEPLOYMENT_FILE):
         with open(settings.DEPLOYMENT_FILE, 'r') as infile:
-            deployment = yaml.load(infile)
+            deployment = yaml.load(infile, Loader=Loader)
         return deployment
     raise FileNotFoundError(f'{settings.DEPLOYMENT_FILE} does not exist')
         
 if __name__=="__main__":
-    deployment = get_deployment()
+    try:
+        deployment = get_deployment()
+        pprint.pprint(deployment)
+        for key,value in deployment.items():
+            pass
+    except FileNotFoundError as e:
+        log.warn(e)
--- deploy/settings.py
+++ deploy/settings.py
@@ -1,10 +1,24 @@
 import os
 from dotenv import load_dotenv
 
-APP_DIR = os.path.dirname(os.path.abspath(__file__))
-PROJECT_DIR = os.path.dirname(os.path.dirname(APP_DIR))
+# DIRECTORY AND FILE CONFIGURATION
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 ENV_DIR = os.path.join(PROJECT_DIR, 'env')
 DEPLOYMENT_FILE = os.path.join(PROJECT_DIR, 'deployments.yml')
+TEMP_DIR=os.path.join(PROJECT_DIR,'templates','stacks')
+# ENVIRONMENT STACKS
+VPC_STACK=os.path.join(TEMP_DIR,'core','vpc.yml')
+RDS_STACK=os.path.join(TEMP_DIR,'core','rds.yml')
+COGNITO_STACK=os.path.join(TEMP_DIR,'core','cognito.yml')
+IAM_STACK=os.path.join(TEMP_DIR,'devops','iam.yml')
+REPO_STACK=os.path.join(TEMP_DIR,'devops','repo.yml')
+CLUSTER_STACK=os.path.join(TEMP_DIR, 'cluster', 'cluster.yml')
+DYNAMO_STACK=os.path.join(TEMP_DIR,'serverless','dynamo.yml')
+CLOUDFRONT_STACK=os.path.join(TEMP_DIR,'serverless','cloudfront.yml')
+LAMBDA_STACK=os.path.join(TEMP_DIR,'serverless','lambda.yml')
+QLDB_STACK=os.path.join(TEMP_DIR,'serverless','qldb.yml')
+# APPLICATION STACKS
 
+# ENVIRONMENT VARIABLES
 if os.path.exists(os.path.join(ENV_DIR, '.env')):
     load_dotenv(os.path.join(ENV_DIR, '.env'))
